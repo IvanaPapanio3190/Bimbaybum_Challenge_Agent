6d3d43921e4fc5b525e834c0ffdc413ce792e06b
IvanaPapanio3190 <123714106+IvanaPapanio3190@users.noreply.github.com>
2026-07-24 01:45:47 -0300

    Versión Final

--- bimbambuy_agent_app.py
+++ bimbambuy_agent_app.py
@@ -100,7 +100,8 @@ if user_input := st.chat_input("Escribí tu consulta aquí..."):
         with st.spinner("Pensando..."):
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash-latest",
+                    model = genai.GenerativeModel('models/gemini-1.5-flash'),
+                   
                     contents=formatted_contents,
                     config=types.GenerateContentConfig(
                         system_instruction=system_instruction,
